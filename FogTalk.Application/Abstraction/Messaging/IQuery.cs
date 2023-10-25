0735a14b9e0edaf70b002ad047835d93bd697098
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-10-25 20:04:14 +0200

    Added IQuery.cs and IQueryHandler.cs abstraction

---
+++ FogTalk.Application/Abstraction/Messaging/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using FogTalk.Domain.Shared;
+using MediatR;
+
+namespace FogTalk.Application.Abstraction.Messaging;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+{
+}
---
+++ FogTalk.Application/Abstraction/Messaging/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using FogTalk.Domain.Shared;
+using MediatR;
+
+namespace FogTalk.Application.Abstraction.Messaging;
+
+public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>> 
+    where TQuery : IQuery<TResponse>
+{
+}

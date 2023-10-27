07e2476e5d85ead12cd21498ac601fb48bc18339
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-10-27 19:50:05 +0200

    DbContext

--- FogTalk.Infrastructure/Persistence/FogTalkDbContext.cs
+++ FogTalk.Infrastructure/Persistence/FogTalkDbContext.cs
@@ -5,18 +5,14 @@ namespace FogTalk.Infrastructure.Persistence;
 
 public class FogTalkDbContext : DbContext
 {
-    // public FogTalkDbContext(DbContextOptions<FogTalkDbContext> options) : base(options)
-    // {
-    // }
-    //
-    // public FogTalkDbContext()
-    // {
-    // }
+    public FogTalkDbContext()
+    {
+    }
     
     public FogTalkDbContext(DbContextOptions options) : base(options)
     {
     }
-
+    
     public DbSet<Chat> Chats { get; set; }
     public DbSet<Message> Messages { get; set; }
     public DbSet<User> Users { get; set; }

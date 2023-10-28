0fb0cf4da38fc81104f0ec5972040cb7cd4feec8
zoneel <40122657+zoneel@users.noreply.github.com>
2023-10-28 17:09:35 +0200

    [Refactor] Result.cs entity

--- FogTalk.Domain/Shared/Result.cs
+++ FogTalk.Domain/Shared/Result.cs
@@ -1,20 +1,33 @@
 ﻿namespace FogTalk.Domain.Shared;
 
-
-public class Result<TValue>
-{
-    public Result(TValue value)
-    {
-        Value = value;
-    }
-
-    public TValue Value { get; }
-}
-
 public class Result 
 {
     public static Result Success()
     {
         return new Result();
+    }
+}
+
+public class Result<T>
+{
+    public bool Succeeded { get; }
+    public T Data { get; }
+    public string ErrorMessage { get; }
+
+    private Result(bool succeeded, T data, string errorMessage)
+    {
+        Succeeded = succeeded;
+        Data = data;
+        ErrorMessage = errorMessage;
+    }
+
+    public static Result<T> Success(T data)
+    {
+        return new Result<T>(true, data, null);
+    }
+
+    public static Result<T> Failure(string errorMessage)
+    {
+        return new Result<T>(false, default(T), errorMessage);
     }
 }

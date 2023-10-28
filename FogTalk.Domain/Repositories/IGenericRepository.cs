e01415b2d540a0214336261e7d554981a7bd1118
zoneel <40122657+zoneel@users.noreply.github.com>
2023-10-28 10:29:48 +0200

    [Added] Generic repository

---
+++ FogTalk.Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+namespace FogTalk.Domain.Repositories;
+
+public interface IGenericRepository<TEntity, in TKey> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(TKey id);
+    public Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, object>>? include = null);
+    Task<Guid> AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+    Task<TEntity?>  GetNextRecordAsync(Expression<Func<TEntity, bool>>filter);
+    public Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+}
---
+++ FogTalk.Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,73 @@
+using System.Linq.Expressions;
+using FogTalk.Domain.Repositories;
+using FogTalk.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace FogTalk.Infrastructure.Repository;
+
+internal class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : class
+{
+    #region ctor and props
+    
+    private readonly FogTalkDbContext _dbContext;
+    private readonly DbSet<TEntity> _entities;
+
+    public GenericRepository(FogTalkDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _entities = _dbContext.Set<TEntity>();
+    }
+    
+    #endregion
+
+    //GetByIdAsync
+    public async Task<TEntity?> GetByIdAsync(TKey id) => await _dbContext.Set<TEntity>().FindAsync(id);
+
+    //GetAllAsync
+    public async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, object>>? include = null)
+    {
+        IQueryable<TEntity> query = _dbContext.Set<TEntity>();
+
+        if (include != null) 
+        {
+            query = query.Include(include);
+        }
+
+        return await query.ToListAsync();
+    }
+    
+    //AddAsync
+    public async Task<Guid> AddAsync(TEntity entity)
+    {
+        await _dbContext.Set<TEntity>().AddAsync(entity);
+        await _dbContext.SaveChangesAsync();
+        var property = _dbContext.Entry(entity).Property("Id");
+        return (Guid)(property.CurrentValue ?? throw new InvalidOperationException());
+    }   
+
+    //UpdateAsync
+    public async Task UpdateAsync(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Update(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    //DeleteAsync
+    public async Task DeleteAsync(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+    
+    //GetNextRecordAsync
+    public async Task<TEntity?> GetNextRecordAsync(Expression<Func<TEntity, bool>> filter)
+    {
+        return await _entities.Where(filter).FirstOrDefaultAsync();
+    }
+    
+    //AnyAsync
+    public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _dbContext.Set<TEntity>().AnyAsync(predicate);
+    }
+}

c6ab230ddedb85cd34a6fe76b67fd5c2dd2efe86
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-10-30 22:36:16 +0100

    [Add] UserRepository

---
+++ FogTalk.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using FogTalk.Domain.Entities;
+using FogTalk.Domain.ValueObjects;
+
+namespace FogTalk.Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task<User> GetByIdAsync(int id);
+    Task<User> GetByEmailAsync(string email);
+    Task<User> GetByUsernameAsync(string username);
+    Task AddAsync(User user);
+}
---
+++ FogTalk.Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using FogTalk.Domain.Entities;
+using FogTalk.Domain.Repositories;
+using FogTalk.Domain.ValueObjects;
+using FogTalk.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace FogTalk.Infrastructure.Repository;
+
+public class UserRepository : IUserRepository
+{
+    private readonly FogTalkDbContext _dbContext; 
+
+    public UserRepository(FogTalkDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<User> GetByIdAsync(int id)
+    {
+        return await _dbContext.Users
+            .FirstOrDefaultAsync(u => u.Id == id);
+    }
+
+    public async Task<User> GetByEmailAsync(string email)
+    {
+        return await _dbContext.Users
+            .FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task<User> GetByUsernameAsync(string username)
+    {
+        return await _dbContext.Users
+            .FirstOrDefaultAsync(u => u.UserName == username);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _dbContext.Users.AddAsync(user);
+        await _dbContext.SaveChangesAsync();
+    }
+}

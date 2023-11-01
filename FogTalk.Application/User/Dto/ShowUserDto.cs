5ca80e261b562a439b06b844e503cb6baf3dd117
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-01 19:50:04 +0100

    [Add] ShowUserDto

---
+++ FogTalk.Application/User/Dto/ShowUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FogTalk.Application.User.Dto;
+
+public class ShowUserDto
+{
+    public string UserName { get; set; } 
+    public string Email { get; set; }
+    public string Bio { get; set; }
+    public string? ProfilePicture { get; set; }
+}

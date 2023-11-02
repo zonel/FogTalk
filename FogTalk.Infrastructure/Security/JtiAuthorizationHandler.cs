0a35278174f86b161aeaead401f9a318baad029e
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-02 15:12:49 +0100

    [Add] Endpoint authorization policy based on JWT's JTI value

---
+++ FogTalk.Infrastructure/Security/JtiAuthorizationHandler.cs
@@ -0,0 +1,31 @@
+﻿using FogTalk.Application.Security;
+using FogTalk.Domain.Repositories;
+using Microsoft.AspNetCore.Authorization;
+    
+namespace FogTalk.Infrastructure.Security;
+
+public class JtiAuthorizationHandler : AuthorizationHandler<JtiRequirement>
+{
+    private readonly IJtiRepository _jtiRepository;
+
+    public JtiAuthorizationHandler(IJtiRepository jtiRepository)
+    {
+        _jtiRepository = jtiRepository;
+    }
+
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, JtiRequirement requirement)
+    {
+        var httpContext = context.Resource as Microsoft.AspNetCore.Http.DefaultHttpContext;
+        
+        if (httpContext.Request.Headers.TryGetValue("Authorization", out var authHeader))
+        {
+            var token = authHeader.ToString().Replace("Bearer ", string.Empty);
+            var jti = _jtiRepository.ExtractJtiFromToken(token);
+            
+            if (!string.IsNullOrEmpty(jti) && !_jtiRepository.IsJtiBlacklisted(jti))
+            {
+                context.Succeed(requirement);
+            }
+        }
+    }
+}
---
+++ FogTalk.Infrastructure/Security/JtiRequirement.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace FogTalk.Application.Security;
+
+public class JtiRequirement : IAuthorizationRequirement { }

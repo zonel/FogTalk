8e7fff5e2c7cf8db1f141d600f1b9a4e793a72e8
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-06 19:06:26 +0100

    [Add] ChatRepository two methods

---
+++ FogTalk.Domain/Repositories/IChatRepository.cs
@@ -0,0 +1,9 @@
+﻿using FogTalk.Domain.Entities;
+
+namespace FogTalk.Domain.Repositories;
+
+public interface IChatRepository
+{
+    public Task<IEnumerable<Chat>> GetChatsForUserAsync(int userId);
+    public Task<Chat> GetChatForUserByIdAsync(int chatId, int userId);
+}
---
+++ FogTalk.Infrastructure/Repository/ChatRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using FogTalk.Domain.Entities;
+using FogTalk.Domain.Repositories;
+using FogTalk.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+public class ChatRepository : IChatRepository
+{
+    private readonly FogTalkDbContext _dbContext; 
+
+    public ChatRepository(FogTalkDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<Chat>> GetChatsForUserAsync(int userId)
+    {
+        return await _dbContext.Chats.Where(c => c.Id == userId).ToListAsync();
+    }
+
+    public async Task<Chat> GetChatForUserByIdAsync(int chatId, int userId)
+    {
+        return await _dbContext.Chats
+            .Include(chat => chat.Participants)
+            .FirstOrDefaultAsync(chat => chat.Id == chatId && chat.Participants.Any(user => user.Id == userId));
+    }
+}

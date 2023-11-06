d984eddd0b421c9954526ff297efa226541b9fba
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-06 19:09:55 +0100

    [Add] GetUserChatsQuery

--- FogTalk.Application/Chat/Queries/GetUserChats/GetUserChatsQueryHandler.cs
+++ FogTalk.Application/Chat/Queries/GetUserChats/GetUserChatsQueryHandler.cs
@@ -11,15 +11,17 @@ public class GetUserChatsQueryHandler : IQueryHandler<GetUserChatsQuery, IEnumer
     #region ctor and props
 
     private readonly IGenericRepository<Domain.Entities.Chat, int> _repository;
-    public GetUserChatsQueryHandler(IGenericRepository<Domain.Entities.Chat, int> repository)
+    private readonly IChatRepository _chatRepository;
+
+    public GetUserChatsQueryHandler(IChatRepository chatRepository)
     {
-        _repository = repository;
+        _chatRepository = chatRepository;
     }
     #endregion
     
     public async Task<IEnumerable<ChatDto>> Handle(GetUserChatsQuery request, CancellationToken cancellationToken)
     {
-        var chats = await _repository.GetAllAsync();
+        var chats = await _chatRepository.GetChatsForUserAsync(request.userId);
         var chatdtos = chats.Adapt<IEnumerable<ChatDto>>();
         return chatdtos;
     }

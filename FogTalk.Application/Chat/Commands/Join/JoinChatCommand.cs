2cc010d679e90ff6c9d524974d6e7aa18240ced0
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-07 15:12:28 +0100

    [Add] JoinChatCommand.cs

---
+++ FogTalk.Application/Chat/Commands/Join/JoinChatCommand.cs
@@ -0,0 +1,7 @@
+﻿using FogTalk.Application.Abstraction.Messaging;
+
+namespace FogTalk.Application.Chat.Commands.Join;
+
+public record JoinChatCommand(int userId, int chatId) : ICommand
+{
+}
---
+++ FogTalk.Application/Chat/Commands/Join/JoinChatCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using FogTalk.Application.Abstraction.Messaging;
+using FogTalk.Domain.Exceptions;
+using FogTalk.Domain.Repositories;
+
+namespace FogTalk.Application.Chat.Commands.Join;
+
+public class JoinChatCommandHandler : ICommandHandler<JoinChatCommand>
+{
+    private readonly IChatRepository _chatRepository;
+
+    public JoinChatCommandHandler(IChatRepository chatRepository)
+    {
+        _chatRepository = chatRepository;
+    }
+    
+    public async Task Handle(JoinChatCommand request, CancellationToken cancellationToken)
+    {
+        //idempotency check
+        if (await _chatRepository.GetChatForUserByIdAsync(request.chatId, request.userId) != null)
+            throw new IdempotencyException("User is already in chat");
+        
+        await _chatRepository.AddUserToChatAsync(request.userId, request.chatId);
+    }
+}

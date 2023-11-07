9711db6866fb114810c17c20723969a4dadc434d
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-07 15:23:38 +0100

    [Add] ChatRepository.cs

--- FogTalk.Domain/Repositories/IChatRepository.cs
+++ FogTalk.Domain/Repositories/IChatRepository.cs
@@ -6,4 +6,8 @@ public interface IChatRepository
 {
     public Task<IEnumerable<Chat>> GetChatsForUserAsync(int userId);
     public Task<Chat> GetChatForUserByIdAsync(int chatId, int userId);
+    public Task AddUserToChatAsync(int userId, int chatId);
+    public Task RemoveUserFromChatAsync(int userId, int chatId);
+    public Task UpdateAsync(Chat chat);
+    public Task DeleteByIdAsync(int id);
 }
--- FogTalk.Infrastructure/Repository/ChatRepository.cs
+++ FogTalk.Infrastructure/Repository/ChatRepository.cs
@@ -15,7 +15,7 @@ public class ChatRepository : IChatRepository
 
     public async Task<IEnumerable<Chat>> GetChatsForUserAsync(int userId)
     {
-        return await _dbContext.Chats.Where(c => c.Id == userId).ToListAsync();
+        return await _dbContext.Chats.Where(c => c.Participants.Any(user => user.Id == userId)).ToListAsync();
     }
 
     public async Task<Chat> GetChatForUserByIdAsync(int chatId, int userId)
@@ -23,5 +23,34 @@ public class ChatRepository : IChatRepository
         return await _dbContext.Chats
             .Include(chat => chat.Participants)
             .FirstOrDefaultAsync(chat => chat.Id == chatId && chat.Participants.Any(user => user.Id == userId));
+    }
+    
+    public async Task AddUserToChatAsync(int userId, int chatId)
+    {
+        var chat = await _dbContext.Chats.Include(c => c.Participants).FirstOrDefaultAsync(c => c.Id == chatId);
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        chat.Participants.Add(user);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task RemoveUserFromChatAsync(int userId, int chatId)
+    {
+        var chat = await _dbContext.Chats.Include(c => c.Participants).FirstOrDefaultAsync(c => c.Id == chatId);
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        chat.Participants.Remove(user);
+        await _dbContext.SaveChangesAsync();
+    }
+    
+    public async Task UpdateAsync(Chat chat)
+    {
+        _dbContext.Chats.Update(chat);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteByIdAsync(int id)
+    {
+        var chatToRemove =  _dbContext.Chats.FirstOrDefault(c => c.Id == id);
+         _dbContext.Chats.Remove(chatToRemove);
+         await _dbContext.SaveChangesAsync();
     }
 }

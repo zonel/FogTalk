bbb07b772596fcd4dd36e0e8e1e634629ee3c1dc
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-07 15:11:48 +0100

    [Add] UpdateChatDto.cs

---
+++ FogTalk.Application/Chat/Dto/UpdateChatDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FogTalk.Application.Chat.Dto;
+
+public class UpdateChatDto
+{
+    public int Id { get; set; }
+    
+    //properties available to update
+    public string Name { get; set; }
+}

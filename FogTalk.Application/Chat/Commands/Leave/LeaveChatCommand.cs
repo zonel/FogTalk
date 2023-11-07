f9253e4fa402667312a54c025e20b3a8ba9e74f5
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-07 15:13:13 +0100

    [Add] LeaveChatCommand.cs

---
+++ FogTalk.Application/Chat/Commands/Leave/LeaveChatCommand.cs
@@ -0,0 +1,5 @@
+﻿using FogTalk.Application.Abstraction.Messaging;
+
+namespace FogTalk.Application.Chat.Commands.Leave;
+
+public record LeaveChatCommand(int userId, int chatId) : ICommand;
---
+++ FogTalk.Application/Chat/Commands/Leave/LeaveChatCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using FogTalk.Application.Abstraction.Messaging;
+using FogTalk.Application.Chat.Commands.Join;
+using FogTalk.Domain.Exceptions;
+using FogTalk.Domain.Repositories;
+
+namespace FogTalk.Application.Chat.Commands.Leave;
+
+public class LeaveChatCommandHandler : ICommandHandler<LeaveChatCommand>
+{
+    private readonly IChatRepository _chatRepository;
+
+    public LeaveChatCommandHandler(IChatRepository chatRepository)
+    {
+        _chatRepository = chatRepository;
+    }
+    
+    public async Task Handle(LeaveChatCommand request, CancellationToken cancellationToken)
+    {
+        //idempotency check
+        if (await _chatRepository.GetChatForUserByIdAsync(request.chatId, request.userId) == null)
+            throw new IdempotencyException("User does not belong to this chat.");
+        
+        await _chatRepository.RemoveUserFromChatAsync(request.userId, request.chatId);
+    }
+
+}

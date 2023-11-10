2bb59a202dbc82592edcb6d50a4fa6e5bea9b089
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-10 21:43:47 +0100

    [Add] GetMessagesInChatCommand

---
+++ FogTalk.Application/Message/Queries/GetAllMessagesInChat/GetMessagesInChatCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿
+using FogTalk.Application.Abstraction.Messaging;
+using FogTalk.Application.Message.Dto;
+using FogTalk.Domain.Repositories;
+
+namespace FogTalk.Application.Message.Queries.GetMessagesInChat;
+
+public class GetMessagesInChatCommandHandler : IQueryHandler<GetMessagesInChatCommand, IEnumerable<ShowMessageDto>>
+{
+    private readonly IMessageRepository _messageRepository;
+
+    public GetMessagesInChatCommandHandler(IMessageRepository messageRepository)
+    {
+        _messageRepository = messageRepository;
+    }
+    public async Task<IEnumerable<ShowMessageDto>> Handle(GetMessagesInChatCommand request, CancellationToken cancellationToken)
+    {
+        return await _messageRepository.GetMessagesAsync<ShowMessageDto>(request.chatId, request.cursor, request.pageSize,cancellationToken);
+    }
+}

eb5e7d0ab4b549b4ed7b552d145aff5a07491524
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-13 21:48:32 +0100

    [Add] SendFriendRequestCommand.cs

--- FogTalk.Application/Friend/Commands/Create/SendFriendRequestCommand.cs
+++ FogTalk.Application/Friend/Commands/Create/SendFriendRequestCommand.cs
@@ -1,6 +1,6 @@
-﻿namespace FogTalk.Application.Friend.Commands.Create;
+﻿using System.Windows.Input;
+using ICommand = FogTalk.Application.Abstraction.Messaging.ICommand;
 
-public class SendFriendRequestCommand
-{
-    
-}
+namespace FogTalk.Application.Friend.Commands.Create;
+
+public record SendFriendRequestCommand(int currentUserId, int receivingUserId) : ICommand;
--- FogTalk.Application/Friend/Commands/Create/SendFriendRequestCommandHandler.cs
+++ FogTalk.Application/Friend/Commands/Create/SendFriendRequestCommandHandler.cs
@@ -1,6 +1,18 @@
-﻿namespace FogTalk.Application.Friend.Commands.Create;
+﻿using FogTalk.Application.Abstraction.Messaging;
+using FogTalk.Domain.Repositories;
 
-public class SendFriendRequestCommandHandler
+namespace FogTalk.Application.Friend.Commands.Create;
+
+public class SendFriendRequestCommandHandler : ICommandHandler<SendFriendRequestCommand>
 {
-    
+    private readonly IFriendRepository _friendRepository;
+
+    public SendFriendRequestCommandHandler(IFriendRepository friendRepository)
+    {
+        _friendRepository = friendRepository;
+    }
+    public async Task Handle(SendFriendRequestCommand request, CancellationToken cancellationToken)
+    {
+        await _friendRepository.SendFriendRequestAsync(request.currentUserId, request.receivingUserId);
+    }
 }

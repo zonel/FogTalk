2363785c45a7227c31d17a1a9dbd438197bde99b
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-13 21:50:35 +0100

    [Add] FriendRepository.cs

---
+++ FogTalk.Domain/Repositories/IFriendRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace FogTalk.Domain.Repositories;
+
+public interface IFriendRepository
+{
+    public Task<IEnumerable<T>> GetUsersFriends<T>(int userId);
+    public IEnumerable<T> GetUsersFriendRequests<T>(int userId);
+    public Task SendFriendRequestAsync(int userId, int friendId); 
+}
---
+++ FogTalk.Infrastructure/Repository/FriendRepository.cs
@@ -0,0 +1,42 @@
+﻿using FogTalk.Domain.Repositories;
+using FogTalk.Infrastructure.Persistence;
+using Mapster;
+
+namespace FogTalk.Infrastructure.Repository;
+
+public class FriendRepository : IFriendRepository
+{
+    private readonly FogTalkDbContext _dbContext;
+
+    public FriendRepository(FogTalkDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<IEnumerable<T>> GetUsersFriends<T>(int userId)
+    {
+        return _dbContext.Users
+            .Where(u => u.Id == userId)
+            .SelectMany(u => u.Friends)
+            .ToList().Adapt<IEnumerable<T>>();
+    }
+    public IEnumerable<T> GetUsersFriendRequests<T>(int userId)
+    {
+        return _dbContext.Users
+            .Where(u => u.Id == userId)
+            .SelectMany(u => u.FriendRequests)
+            .ToList().Adapt<IEnumerable<T>>();
+    }
+
+    public async Task SendFriendRequestAsync(int userId, int friendId)
+    {
+        var receivingUser = await _dbContext.Users.FindAsync(friendId);
+        
+        _dbContext.Users
+            .Where(u => u.Id == userId)
+            .SelectMany(u => u.FriendRequests)
+            .ToList()
+            .Add(receivingUser);
+        
+        await _dbContext.SaveChangesAsync();
+    }
+}

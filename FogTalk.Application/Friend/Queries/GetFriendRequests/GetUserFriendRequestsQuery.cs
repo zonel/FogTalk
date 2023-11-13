f267471589748de9fc7e2284bf73a09167c5607a
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-13 21:48:49 +0100

    [Add] GetUserFriendRequestsQuery.cs

---
+++ FogTalk.Application/Friend/Queries/GetFriendRequests/GetUserFriendRequestsQuery.cs
@@ -0,0 +1,6 @@
+﻿using FogTalk.Application.Abstraction.Messaging;
+using FogTalk.Application.Friend.Dto;
+
+namespace FogTalk.Application.Friend.Queries.GetFriendRequests;
+
+public record GetUserFriendRequestsQuery(int userId) : IQuery<IEnumerable<ShowFriendRequestDto>>;
---
+++ FogTalk.Application/Friend/Queries/GetFriendRequests/GetUserFriendRequestsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using FogTalk.Application.Abstraction.Messaging;
+using FogTalk.Application.Friend.Dto;
+using FogTalk.Domain.Repositories;
+
+namespace FogTalk.Application.Friend.Queries.GetFriendRequests;
+
+public class GetUserFriendRequestsQueryHandler : IQueryHandler<GetUserFriendRequestsQuery, IEnumerable<ShowFriendRequestDto>>
+{
+    private readonly IFriendRepository _friendRepository;
+
+    public GetUserFriendRequestsQueryHandler(IFriendRepository friendRepository)
+    {
+        _friendRepository = friendRepository;
+    }
+    
+    public async Task<IEnumerable<ShowFriendRequestDto>> Handle(GetUserFriendRequestsQuery request, CancellationToken cancellationToken)
+    {
+        return _friendRepository.GetUsersFriendRequests<ShowFriendRequestDto>(request.userId);
+    }
+}

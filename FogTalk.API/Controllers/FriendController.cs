d0cbefb8ea96a09b68b0f683bdd451a737692d46
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-13 21:51:08 +0100

    [Add] Endpoints to FriendController

--- FogTalk.API/Controllers/FriendController.cs
+++ FogTalk.API/Controllers/FriendController.cs
@@ -1,10 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FogTalk.Application.Friend.Commands.Create;
+using FogTalk.Application.Friend.Dto;
+using FogTalk.Application.Friend.Queries.Get;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace FogTalk.API.Controllers;
 
-[Route("api/[controller]")]
 [ApiController]
+[Authorize(Policy = "JtiPolicy")]
+[Route("api/[controller]")]
 public class FriendController : ControllerBase
 {
+    private readonly IMediator _mediator;
+
+    public FriendController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
     
+    [HttpGet]
+    public async Task<IEnumerable<ShowFriendDto>> Get()
+    {
+        var userId = HttpContext.User.FindFirst(JwtRegisteredClaimNames.Sid).Value;
+        var friends = await _mediator.Send(new GetUserFriendsQuery(Convert.ToInt32(userId)));
+        return friends;
+    }
+    
+    [HttpGet("requests")]
+    public async Task<IEnumerable<ShowFriendDto>> GetFriendRequests()
+    {
+        var userId = HttpContext.User.FindFirst(JwtRegisteredClaimNames.Sid).Value;
+        var friends = await _mediator.Send(new GetUserFriendsQuery(Convert.ToInt32(userId)));
+        return friends;
+    }
+        
+    [HttpGet("{receivingUserId}")]
+    public async Task<IActionResult> SendFriendRequest([FromRoute] int receivingUserId)
+    {
+        var currentUserId = HttpContext.User.FindFirst(JwtRegisteredClaimNames.Sid).Value;
+        await _mediator.Send(new SendFriendRequestCommand(Convert.ToInt32(currentUserId), receivingUserId));
+        return Ok();
+    }
+
 }

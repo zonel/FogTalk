c3eaca2f1ae68d0e8dabf70e37c6c51c35b69b66
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-15 21:02:50 +0100

    [Add] FriendMappings

---
+++ FogTalk.Application/Friend/Mappings/FriendMapping.cs
@@ -0,0 +1,13 @@
+﻿using FogTalk.Application.Chat.Dto;
+using FogTalk.Application.User.Dto;
+using Mapster;
+
+namespace FogTalk.Application.Friend.Mappings;
+public class FriendMappings : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.ForType<IEnumerable<Domain.Entities.User>, IEnumerable<ShowUserDto>>();
+        config.ForType<IEnumerable<ShowUserDto>, IEnumerable<Domain.Entities.User>>();
+    }
+}

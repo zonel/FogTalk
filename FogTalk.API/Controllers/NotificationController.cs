33b66909dded848e929c8420a383a23b5abcce54
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-17 18:50:08 +0100

    [Add] NotificationController endpoints

--- FogTalk.API/Controllers/NotificationController.cs
+++ FogTalk.API/Controllers/NotificationController.cs
@@ -1,6 +1,40 @@
-﻿namespace FogTalk.API.Controllers;
+﻿using FogTalk.Application.Friend.Dto;
+using FogTalk.Application.Message.Dto;
+using FogTalk.Application.Notification.Commands.Update;
+using FogTalk.Application.Notification.Dto;
+using FogTalk.Application.Notification.Queries.Get;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
 
-public class NotificationController
+namespace FogTalk.API.Controllers;
+
+[ApiController]
+[Authorize(Policy = "JtiPolicy")]
+[Route("api/[controller]")]
+public class NotificationController : ControllerBase
 {
+    private readonly IMediator _mediator;
+
+    public NotificationController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<NotificationDto>> Get()
+    {
+        var userId = HttpContext.User.FindFirst(JwtRegisteredClaimNames.Sid).Value;
+        var notifications = await _mediator.Send(new GetNotificationsForUserQuery(Convert.ToInt32(userId)));
+        return notifications ?? new List<NotificationDto>();
+    }
+    
+    [HttpPost("{notificationId}")]
+    public async Task MarkAsRead([FromRoute] int notificationId)
+    {
+        var userId = HttpContext.User.FindFirst(JwtRegisteredClaimNames.Sid).Value;
+        await _mediator.Send(new MarkNotificationAsReadCommand(Convert.ToInt32(userId), notificationId));
+    }
     
 }
---
+++ FogTalk.Infrastructure/EntitiesConfiguration/NotificationConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using FogTalk.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FogTalk.Infrastructure.EntitiesConfiguration;
+
+public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+{
+    public void Configure(EntityTypeBuilder<Notification> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Message).IsRequired();
+        builder.Property(u => u.User).IsRequired();
+        builder.Property(u => u.CreatedAt).IsRequired();
+        builder.Property(u => u.IsRead).IsRequired();
+        
+        builder
+            .HasOne(u => u.User)
+            .WithMany(u => u.Notifications)
+            .HasForeignKey(u => u.UserId);
+    }
+}

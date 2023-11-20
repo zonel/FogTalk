c9b4511ecfcc0499431b768d714768cc4d2a40ed
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-20 22:54:16 +0100

    [Refactor] removed obsolete code

--- FogTalk.Infrastructure/EntitiesConfiguration/NotificationConfiguration.cs
+++ FogTalk.Infrastructure/EntitiesConfiguration/NotificationConfiguration.cs
@@ -10,15 +10,7 @@ public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
     {
         builder.HasKey(u => u.Id);
         builder.Property(u => u.Message).IsRequired();
-        //builder.Property(u => u.User).IsRequired();
         builder.Property(u => u.CreatedAt).IsRequired();
         builder.Property(u => u.IsRead).IsRequired();
-        
-        //Owner
-        // builder
-        //     .HasOne(n => n.User)
-        //     .WithMany(u => u.ReceivedNotifications)
-        //     .HasForeignKey(n => n.UserId)
-        //     .OnDelete(DeleteBehavior.Restrict);
     }
 }
